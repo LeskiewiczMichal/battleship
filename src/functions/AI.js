0ee8717289b1071d9ec35c7e804717a6ecc0f5a8
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-08-08 10:59:37 +0200

    Computer tries all fields, disabled button bugs it

--- src/functions/AI.js
+++ src/functions/AI.js
@@ -1,5 +1,34 @@
 // TODO AFTER HIT TRY SIMILAR COORDS
+
+// Number of tried fields around hit ship
+const numOfTries = {
+  firstTry: false,
+  secondTry: false,
+  thirdTry: false,
+};
+
+function countTry() {
+  if (numOfTries.firstTry === true && numOfTries.secondTry === true) {
+    numOfTries.thirdTry = true;
+  } else if (numOfTries.firstTry === true && numOfTries.secondTry === false) {
+    numOfTries.secondTry = true;
+  } else if (numOfTries.firstTry === false) {
+    numOfTries.firstTry = true;
+  }
+}
+
+function getNumOfTries() {
+  return numOfTries;
+}
+
+function resetNumOfTries() {
+  numOfTries.firstTry = false;
+  numOfTries.secondTry = false;
+  numOfTries.thirdTry = false;
+}
+// FIELDCHOSEN USED FOR NEXT MOVE IF SHIP WAS HIT
 let fieldChosen = null;
+
 function setFieldChosen(allFields, indexOfLastField, indexToCheckNext) {
   const nextFieldIndex = indexOfLastField + indexToCheckNext;
   fieldChosen = allFields[nextFieldIndex];
@@ -11,6 +40,17 @@ function setFieldChosenNull() {
 
 function getFieldChosen() {
   return fieldChosen;
+}
+
+// FIRST FIELD WITH SHIP ON IT PARAM; CHECK IF THE SHIP IS SUNK AND IF NOT TRY OTHER FIELDS
+let firstHitParam = null;
+
+function setFirstHitParam(index) {
+  firstHitParam = index;
+}
+
+function getFirstHitParam() {
+  return firstHitParam;
 }
 
 const randomAttack = (playerFields, gameboardArray) => {
@@ -27,7 +67,17 @@ const randomAttack = (playerFields, gameboardArray) => {
         indexOfField = i;
       }
     }
+    if (
+      gameboardArray[indexOfField].hasShip === true &&
+      gameboardArray[indexOfField].shipSunk === false
+    ) {
+      setFirstHitParam(indexOfField);
+      setFieldChosen(fields, indexOfField, 1);
+    } else {
+      setFieldChosenNull();
+    }
   } else {
+    // clicks chosen field
     const field = getFieldChosen();
     for (let i = 0; i < fields.length; i++) {
       if (fields[i] === field) {
@@ -35,16 +85,51 @@ const randomAttack = (playerFields, gameboardArray) => {
       }
     }
     field.click();
+
+    if (
+      gameboardArray[indexOfField].hasShip === true &&
+      gameboardArray[indexOfField].shipSunk === false
+    ) {
+      setFieldChosen(fields, indexOfField, 1);
+    } else if (
+      gameboardArray[indexOfField].hasShip === true &&
+      gameboardArray[indexOfField].shipSunk === true
+    ) {
+      setFieldChosenNull();
+    } else {
+      const numOfTries = getNumOfTries();
+      const firstHitIndex = getFirstHitParam();
+      if (numOfTries.thirdTry === true) {
+        setFieldChosenNull();
+        setFirstHitParam(null);
+        resetNumOfTries();
+      } else if (
+        numOfTries.thirdTry === false &&
+        numOfTries.secondTry === true
+      ) {
+        setFieldChosen(fields, firstHitIndex, -1);
+        countTry();
+      } else if (
+        numOfTries.secondTry === false &&
+        numOfTries.firstTry === true
+      ) {
+        setFieldChosen(fields, firstHitIndex, 10);
+        countTry();
+      } else if (numOfTries.firstTry === false) {
+        setFieldChosen(fields, firstHitIndex, -10);
+        countTry();
+      }
+    }
   }
   // if gameboardArray[indexOfField] hasShip: true, isSunk: false set nearby field for use
-  if (
-    gameboardArray[indexOfField].hasShip === true &&
-    gameboardArray[indexOfField].shipSunk === false
-  ) {
-    setFieldChosen(fields, indexOfField, 1);
-  } else {
-    setFieldChosenNull();
-  }
+  //   if (
+  //     gameboardArray[indexOfField].hasShip === true &&
+  //     gameboardArray[indexOfField].shipSunk === false
+  //   ) {
+  //     setFieldChosen(fields, indexOfField, 1);
+  //   } else {
+  //     setFieldChosenNull();
+  //   }
 };
 
 function isNotShotAlready(field) {
